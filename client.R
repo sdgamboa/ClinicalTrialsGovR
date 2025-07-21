8d2ec5ea52e0c34ce8efab876bde6009ee0040a2
sdgamboa <samuel.gamboa.tuz@gmail.com>
2025-07-20 22:30:07 -0600

    Add code for getting version

--- client.R
+++ client.R
@@ -1,6 +1,7 @@
 
 library(httr2)
 
+## GET Studies ####
 apiServerBeta <- "https://beta-ut.clinicaltrials.gov/api/v2"
 req <- request(base_url = apiServerBeta) |> 
     req_url_path_append("studies/NCT00841061") |> 
@@ -12,10 +13,22 @@ resp <- req |>
     req_perform()
 json_data <- resp_body_json(resp)
 
+json_data$studies
+
+## GET Stats ####
+
+## GET Version ####
+req <- request(base_url = apiServerBeta) |> 
+    req_url_path_append("version")
+resp <- req |> 
+    req_perform()
+
+json_data <- resp_body_json(resp)
+
+purrr::reduce(.x = json_data, .f = ~ paste(.x, .y, sep = " "))
+
+## Explore ####
 req |> 
     req_dry_run()
 resp |>
     resp_raw()
-
-json_data$studies
-
